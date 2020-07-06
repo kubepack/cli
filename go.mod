a6e32bc312c32bec086bcdc3d21fe80a68cf252e
Tamal Saha <tamal@appscode.com>
2020-07-06 10:25:40 -0700

    added-all

--- go.mod
+++ go.mod
@@ -5,7 +5,10 @@ go 1.14
 require (
 	github.com/appscode/go v0.0.0-20200323182826-54e98e09185a
 	github.com/jonboulle/clockwork v0.2.0
+	github.com/pkg/errors v0.9.1
 	github.com/spf13/cobra v1.0.0
+	github.com/spf13/pflag v1.0.5
+	helm.sh/helm/v3 v3.0.0-00010101000000-000000000000
 	k8s.io/api v0.18.5
 	k8s.io/apimachinery v0.18.5
 	k8s.io/cli-runtime v0.18.5
@@ -16,6 +19,8 @@ require (
 	k8s.io/kubectl v0.18.5
 	k8s.io/utils v0.0.0-20200324210504-a9aa75ae1b89
 	kmodules.xyz/client-go v0.0.0-20200630053911-20d035822d35
+	rsc.io/letsencrypt v0.0.3 // indirect
+	sigs.k8s.io/yaml v1.2.0
 )
 
 replace github.com/googleapis/gnostic => github.com/googleapis/gnostic v0.2.0
--- root.go
+++ root.go
@@ -2,15 +2,44 @@ package main
 
 import (
 	"flag"
-
-	"github.com/tamalsaha/k-apply/apply"
+	"fmt"
+	"io/ioutil"
+	"log"
+	"os"
+	"strings"
 
 	"github.com/spf13/cobra"
+	"helm.sh/helm/v3/pkg/action"
+	"helm.sh/helm/v3/pkg/cli"
+	"helm.sh/helm/v3/pkg/release"
+	"helm.sh/helm/v3/pkg/storage/driver"
+	"sigs.k8s.io/yaml"
+
+	// Import to initialize client auth plugins.
+	"github.com/spf13/cobra"
+	"github.com/tamalsaha/k-apply/apply"
+	"helm.sh/helm/v3/pkg/action"
+	"helm.sh/helm/v3/pkg/cli"
+	kubefake "helm.sh/helm/v3/pkg/kube/fake"
+	"helm.sh/helm/v3/pkg/release"
+	"helm.sh/helm/v3/pkg/storage/driver"
 	"k8s.io/cli-runtime/pkg/genericclioptions"
+	_ "k8s.io/client-go/plugin/pkg/client/auth"
 	cliflag "k8s.io/component-base/cli/flag"
 	cmdutil "k8s.io/kubectl/pkg/cmd/util"
 	"kmodules.xyz/client-go/logs"
 )
+
+var (
+	settings = cli.New()
+)
+
+func debug(format string, v ...interface{}) {
+	if settings.Debug {
+		format = fmt.Sprintf("[debug] %s\n", format)
+		log.Output(2, fmt.Sprintf(format, v...))
+	}
+}
 
 func NewRootCmd() *cobra.Command {
 	var rootCmd = &cobra.Command{
@@ -38,5 +67,56 @@ func NewRootCmd() *cobra.Command {
 	ioStreams, _, _, _ := genericclioptions.NewTestIOStreams()
 	rootCmd.AddCommand(apply.NewCmdApply("k-apply", f, ioStreams))
 
+	actionConfig := new(action.Configuration)
+	settings.AddFlags(flags)
+	helmDriver := os.Getenv("HELM_DRIVER")
+	if err := actionConfig.Init(settings.RESTClientGetter(), settings.Namespace(), helmDriver, debug); err != nil {
+		log.Fatal(err)
+	}
+	if helmDriver == "memory" {
+		loadReleasesInMemory(actionConfig)
+	}
+	rootCmd.AddCommand(newInstallCmd(actionConfig, os.Stdout))
+	rootCmd.AddCommand(newUpgradeCmd(actionConfig, os.Stdout))
+	rootCmd.AddCommand(newUninstallCmd(actionConfig, os.Stdout))
+
 	return rootCmd
+}
+
+// This function loads releases into the memory storage if the
+// environment variable is properly set.
+func loadReleasesInMemory(actionConfig *action.Configuration) {
+	filePaths := strings.Split(os.Getenv("HELM_MEMORY_DRIVER_DATA"), ":")
+	if len(filePaths) == 0 {
+		return
+	}
+
+	store := actionConfig.Releases
+	mem, ok := store.Driver.(*driver.Memory)
+	if !ok {
+		// For an unexpected reason we are not dealing with the memory storage driver.
+		return
+	}
+
+	actionConfig.KubeClient = &kubefake.PrintingKubeClient{Out: ioutil.Discard}
+
+	for _, path := range filePaths {
+		b, err := ioutil.ReadFile(path)
+		if err != nil {
+			log.Fatal("Unable to read memory driver data", err)
+		}
+
+		releases := []*release.Release{}
+		if err := yaml.Unmarshal(b, &releases); err != nil {
+			log.Fatal("Unable to unmarshal memory driver data: ", err)
+		}
+
+		for _, rel := range releases {
+			if err := store.Create(rel); err != nil {
+				log.Fatal(err)
+			}
+		}
+	}
+	// Must reset namespace to the proper one
+	mem.SetNamespace(settings.Namespace())
 }
